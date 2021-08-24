72c67daf674b63272724f21d319db61f3e223e86
Bijon Santra <34066467+bijonsantra@users.noreply.github.com>
2021-08-24 09:57:15 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_s3_bucket" "drive" {
-  bucket = "my-tf-test-bucket"
+  bucket = "bijon_tf_test_bucket"
   acl    = "public-read"
   tags = {
     Name        = var.bucket_name
